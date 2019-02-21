4900a45bae14c2c37b3a177cc1fdeea707aee2ee
zhangqinghao <zhangqinghao@123juzi.com>
2019-02-21 15:25:05 +0800

    添加shell命令行基础笔记 完善shell命令行；实现自动化部署

--- updata_to_git.sh
+++ updata_to_git.sh
@@ -1,6 +1,7 @@
 hexo clean
 hexo generate
 cp -R public/* ../git_io_blob/haohualiangshi.github.io
+cd ../git_io_blob/haohualiangshi.github.io
 git add .
 git commit -m "update"
 git push origin master -u
